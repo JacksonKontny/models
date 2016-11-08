ae70e0aef32a63a55d3a449c722ab2ab9aaa57d2
Jackson <jackson.kontny@analytehealth.com>
2016-11-07 19:42:17 -0600

    0001 - generalize preprocessing

--- inception/inception/data/preprocess_lidc_data.py
+++ inception/inception/data/preprocess_lidc_data.py
@@ -1,4 +1,3 @@
-import npath
 import shutil
 import random
 import dicom
@@ -11,7 +10,11 @@ import sqlite3
 
 
 db_filename = '/Users/jacksonkontny/Projects/DePaul/csc481/CT_norm_002.sql3'
-dicom_path = '/Users/jacksonkontny/Projects/DePaul/csc481/LIDC/LIDC_IRDI/'
+dicom_path = '/Users/jacksonkontny/Projects/DePaul/csc481/' # LIDC/LIDC_IRDI/'
+TRAINING_DATA_DIR = 'raw_data'
+CLASSIFICATION_TYPES = ['malignant', 'benign']
+TRAINING_SETS = ['train', 'validation']
+
 
 def get_lidc_files(lidc_path):
     dcm_files_list = []  # create an empty list
@@ -21,16 +24,49 @@ def get_lidc_files(lidc_path):
                 dcm_files_list.append(os.path.join(dirName, filename))
     return dcm_files_list
 
+def create_or_clean_data_directories(dicom_Path):
+    ''' creates directories for each training set and subdirectories for each
+        classification type under each training set, for a total of trainig
+        set * classification type directores.
+
+        CAUTION: this will delete all training data you have populated in the
+        training and classification directories.
+    '''
+    def create_or_clean(dir_name):
+        if not os.path.isdir(dir_name):
+            os.mkdir(dir_name)
+        else:
+            shutil.rmtree(dir_name)
+            os.mkdir(dir_name)
+    data_dir_name = os.path.join(dicom_path, TRAINING_DATA_DIR)
+    create_or_clean(data_dir_name)
+    for training_dir in TRAINING_SETS:
+        training_dir_name = os.path.join(data_dir_name, training_dir)
+        create_or_clean(training_dir_name)
+        for classification in CLASSIFICATION_TYPES:
+            classification_dir_name = os.path.join(training_dir_name, classification)
+            create_or_clean(classification_dir_name)
+
+def create_training_and_validation_sets(preprocessed_files, training_pct=.95):
+    training = random.sample(lidc_files, int(len(lidc_files) * training_pct))
+    validation = set(lidc_files) - set(training)
+    return list(training), list(validation)
+
 def process_lidc_files(lidc_files, conn):
-    preprocessed_files = []
-    for lidc_file in lidc_files:
-        dc = dicom.read_file(lidc_file , force=True)
-        classification = get_classification(dc.SOPInstanceUID, conn)
-        px_array = dc.pixel_array
-        out_file_name = '%sraw_data/%s/%s_%s.jpeg' % (dicom_path, classification, str(dc.PatientID), str(dc.InstanceNumber))
-        pylab.imsave(out_file_name, px_array, cmap=cm.Greys_r)
-        preprocessed_files.append(out_file_name)
-    return preprocessed_files
+    data_dir = os.path.join(dicom_path, TRAINING_DATA_DIR)
+    def process_file_set(data_set, data_set_name):
+        for lidc_file in data_set:
+            dc = dicom.read_file(lidc_file , force=True)
+            classification = get_classification(dc.SOPInstanceUID, conn)
+            classification_dir = os.path.join(data_dir, data_set_name, classification)
+            px_array = dc.pixel_array
+            out_file_name = '{}_{}.jpeg'.format(str(dc.PatientID), str(dc.InstanceNumber))
+            out_file_path = os.path.join(classification_dir, out_file_name)
+            pylab.imsave(out_file_path, px_array, cmap=cm.Greys_r)
+
+    training_set, validation_set = create_training_and_validation_sets(lidc_files)
+    process_file_set(training_set, 'train')
+    process_file_set(validation_set, 'validation')
 
 def get_classification(image_sop_uid, conn):
     cursor = conn.cursor()
@@ -43,16 +79,9 @@ def get_classification(image_sop_uid, conn):
     count = cursor.fetchone()[0]
     return 'malignant' if count else 'benign'
 
-def create_training_and_validation_sets(preprocessed_files):
-    training = random.sample(lidc_files, int(len(lidc_files) * .95))
-    validation = set(lidc_files) - set(training)
-    for fn in training:
-        shutil.move(fn, '%sraw_data/train/%s' % (dicom_path, npath.basename(fn)))
-    for fn in validation:
-        shutil.move(fn, '%sraw_data/validation/%s' % (dicom_path, npath.basename(fn)))
 
 if __name__ == "__main__":
     lidc_files = get_lidc_files(dicom_path)
+    create_or_clean_data_directories(dicom_path)
     with sqlite3.connect(db_filename) as conn:
-        preprocessed_files = process_lidc_files(lidc_files, conn)
-    create_training_and_validation_sets(preprocessed_files)
+        process_lidc_files(lidc_files, conn)
