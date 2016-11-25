e5a59665caf165f9b5245e47f7cd156bf03f929b
Jackson <jackson.kontny@analytehealth.com>
2016-11-24 21:05:48 -0700

    0001_init - preprocessing cleaning

--- inception/inception/data/preprocess_lidc_data.py
+++ inception/inception/data/preprocess_lidc_data.py
@@ -9,22 +9,27 @@ import pylab
 import sqlite3
 
 
-db_filename = '/Users/jacksonkontny/Projects/DePaul/csc481/CT_norm_002.sql3'
-dicom_path = '/Users/jacksonkontny/Projects/DePaul/csc481/' # LIDC/LIDC_IRDI/'
-TRAINING_DATA_DIR = 'raw_data'
+BASE_PATH = '/Users/jacksonkontny/Projects/DePaul/csc481/'
+DATA_PATH = os.path.join(BASE_PATH, 'LIDC', 'LIDC_IDRI')
+DB_FILENAME = os.path.join(BASE_PATH, 'CT_norm_002.sql3')
+RAW_DATA_PATH = os.path.join(DATA_PATH, 'raw_data')
+PREPROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'preprocessed_data')
 CLASSIFICATION_TYPES = ['malignant', 'benign']
 TRAINING_SETS = ['train', 'validation']
+NUM_SAMPLES = 1000 # set to a number if you only want to get a small sample
 
 
-def get_lidc_files(lidc_path):
+def get_lidc_files():
     dcm_files_list = []  # create an empty list
-    for dirName, subdirList, fileList in os.walk(lidc_path):
+    for dirName, subdirList, fileList in os.walk(RAW_DATA_PATH):
         for filename in fileList:
             if ".dcm" in filename.lower():  # check whether the file's DICOM
                 dcm_files_list.append(os.path.join(dirName, filename))
+    if NUM_SAMPLES:
+        dcm_files_list = dcm_files_list[:NUM_SAMPLES]
     return dcm_files_list
 
-def create_or_clean_data_directories(dicom_Path):
+def create_or_clean_data_directories():
     ''' creates directories for each training set and subdirectories for each
         classification type under each training set, for a total of trainig
         set * classification type directores.
@@ -38,10 +43,9 @@ def create_or_clean_data_directories(dicom_Path):
         else:
             shutil.rmtree(dir_name)
             os.mkdir(dir_name)
-    data_dir_name = os.path.join(dicom_path, TRAINING_DATA_DIR)
-    create_or_clean(data_dir_name)
+    create_or_clean(PREPROCESSED_DATA_PATH)
     for training_dir in TRAINING_SETS:
-        training_dir_name = os.path.join(data_dir_name, training_dir)
+        training_dir_name = os.path.join(PREPROCESSED_DATA_PATH, training_dir)
         create_or_clean(training_dir_name)
         for classification in CLASSIFICATION_TYPES:
             classification_dir_name = os.path.join(training_dir_name, classification)
@@ -50,15 +54,14 @@ def create_or_clean_data_directories(dicom_Path):
 def create_training_and_validation_sets(preprocessed_files, training_pct=.95):
     training = random.sample(lidc_files, int(len(lidc_files) * training_pct))
     validation = set(lidc_files) - set(training)
-    return list(training), list(validation)
+    return sorted(list(training)), sorted(list(validation))
 
 def process_lidc_files(lidc_files, conn):
-    data_dir = os.path.join(dicom_path, TRAINING_DATA_DIR)
     def process_file_set(data_set, data_set_name):
         for lidc_file in data_set:
             dc = dicom.read_file(lidc_file , force=True)
             classification = get_classification(dc.SOPInstanceUID, conn)
-            classification_dir = os.path.join(data_dir, data_set_name, classification)
+            classification_dir = os.path.join(PREPROCESSED_DATA_PATH, data_set_name, classification)
             px_array = dc.pixel_array
             out_file_name = '{}_{}.jpeg'.format(str(dc.PatientID), str(dc.InstanceNumber))
             out_file_path = os.path.join(classification_dir, out_file_name)
@@ -81,7 +84,7 @@ def get_classification(image_sop_uid, conn):
 
 
 if __name__ == "__main__":
-    lidc_files = get_lidc_files(dicom_path)
-    create_or_clean_data_directories(dicom_path)
-    with sqlite3.connect(db_filename) as conn:
+    lidc_files = get_lidc_files()
+    create_or_clean_data_directories()
+    with sqlite3.connect(DB_FILENAME) as conn:
         process_lidc_files(lidc_files, conn)
