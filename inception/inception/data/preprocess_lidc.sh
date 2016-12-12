885f188389eec9701b7769e04789d715681383a3
Jackson <jackson.kontny@analytehealth.com>
2016-12-12 08:10:32 -0600

    0003_even - set preprocessing directory

--- inception/inception/data/preprocess_lidc.sh
+++ inception/inception/data/preprocess_lidc.sh
@@ -18,7 +18,7 @@ set -e
 
 <<<<<<< c2614f69d88ee2370b50079a811fe6c9eee5974d
 BASE_DIR="${1%/}"
-EXPERIMENT_NAME="base"
+EXPERIMENT_NAME="even_split"
 RAW_DATA_DIR="${BASE_DIR}/LIDC_IDRI"
 DB_FILENAME="${BASE_DIR}/sqlite_db/CT_norm_002.sql3"
 PREPROCESSED_DATA_OUTPUT_PATH="${BASE_DIR}/inception/${EXPERIMENT_NAME}/preprocessed_data/"
